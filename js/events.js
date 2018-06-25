10566ea06f779564939442f06eb3ca3c5939f4c1
Nick H <nhuntington11@gmail.com>
2018-06-25 10:40:01 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -12,7 +12,7 @@ function frameIt() {
 
 function pressIt() {
   $('input').on('keydown', function(e) {
-    if (e.key === 'g') {
+    if (e.which === 71) {
       alert('You have pressed "g"')
     }
   })
